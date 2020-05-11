bd99c022993bf479f7b360636629a424ee2db780
jinalee <spzn02@gmail.com>
2020-05-11 23:52:45 +0900

    upload 0511

--- vue-router_4/src/views/About.vue
+++
@@ -1,10 +0,0 @@
-<template>
-  <div class="text-xs-center">
-    This is About Page
-    <h1>클릭</h1>
-    <router-link :to="{name : 'home'}">
-      <h1>클릭</h1>
-    </router-link>
-  
-  </div>
-</template>
